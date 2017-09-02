657a7b0ca3e0442a5b856bfa089d1df6b701cf70
yaojianzhao <1720231298.qq.com>
2017-09-02 20:53:06 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by yaojianzhao on 2017/9/2.
+ */
+import React from 'react'
+
+export default React.createClass({
+    render: function() {
+        return <div>Hello React!</div>
+    }
+})
